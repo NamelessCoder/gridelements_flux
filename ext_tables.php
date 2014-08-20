972a17662c10606b63753160c30e14785449ba91
Claus Due <claus@namelesscoder.net>
2014-08-20 21:35:25 +0200

    [TASK] Unset gridelements' flexform integration fields
    Allows the CType gridelements_pi1 to be operated with a Flux Provider as DS source renderer.

--- ext_tables.php
+++ ext_tables.php
@@ -18,3 +18,18 @@ if (!defined('TYPO3_MODE')) {
 	)
 ));
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tx_gridelements_backend_layout', 'tx_gridelementsflux_templatesource,tx_gridelementsflux_templatefile');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content', 'pi_flexform', 'gridelements_pi1');
+
+$hookIndex = array_search(
+	'EXT:gridelements/Classes/Hooks/BackendUtility.php:GridElementsTeam\\Gridelements\\Hooks\\BackendUtility',
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_befunc.php']['getFlexFormDSClass']
+);
+unset(
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_befunc.php']['getFlexFormDSClass'][$hookIndex],
+	$GLOBALS['TCA']['tt_content']['columns']['pi_flexform']['config']['ds']['*,gridelements_pi1'],
+	$GLOBALS['TCA']['tt_content']['columns']['pi_flexform_ds'],
+	$GLOBALS['TCA']['tt_content']['columns']['pi_flexform_ds_file'],
+	$hookIndex
+);
+
+
