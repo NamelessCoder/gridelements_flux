11e7b00b6be37ecd02ae66a96802368c8296cc39
Claus Due <claus@namelesscoder.net>
2014-08-20 20:35:33 +0200

    [TASK] Add SQL schema alterations

---
+++ ext_tables.sql
@@ -0,0 +1,7 @@
+#
+# Table structure for table 'tx_gridelements_backend_layout'
+#
+CREATE TABLE tx_gridelements_backend_layout (
+	tx_gridelementsflux_templatesource text,
+	tx_gridelementsflux_templatefile text
+)
