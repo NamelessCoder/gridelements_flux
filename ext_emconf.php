ce4deee359e630ea65adf1f612c204d866a90e28
Claus Due <claus@namelesscoder.net>
2014-08-20 20:18:58 +0200

    [TASK] Add basic extension files

---
+++ ext_emconf.php
@@ -0,0 +1,53 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "gridelements_flux".
+ *
+ * Auto generated 08-20-2014 20:12
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Gridelements Flux integration',
+	'description' => 'Provides a way to use Flux forms in Gridelements',
+	'category' => 'misc',
+	'shy' => 0,
+	'version' => '0.1.0',
+	'dependencies' => 'cms,extbase,fluid,gridelements',
+	'conflicts' => '',
+	'priority' => 'bottom',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Claus Due',
+	'author_email' => 'claus@namelesscoder.net',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.1.0-6.2.99',
+			'cms' => '',
+			'extbase' => '',
+			'fluid' => '',
+			'gridelements' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:0:{}',
+	'suggests' => array(
+	),
+);
+
+?>
